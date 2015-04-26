b081ca26959b8608241319dc2f91923ec5d59e53
Lago <jclagomacini@gmail.com>
2015-04-26 13:45:17 +0200

    last update

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,16 @@
+## Script used to transform the UCI HAR Dataset in tidy data
 
-## This code make tidy dataset from original "UCI HAR Dataset" 
-
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation 
-##    for each measurement. 
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive variable names. 
-## 5. From the data set in step 4, creates a second, independent tidy data set 
-##    with the average of each variable for each activity and each subject.
-
+## Packages
 library(dplyr)
 library(tidyr)
 library(reshape2)
 
+### Variables to name the variables and labels of the data frame
 features <- read.table("./UCI HAR Dataset/features.txt")
 activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 names(activities) <- c("index", "activities")
 
+### Downloading "test" and "train" datasets 
 Table_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
 test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
@@ -28,28 +22,31 @@ train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
 train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 Table_train <- cbind(train_subject, train_labels, Table_train)
 
+## Creating Table1. Merging both dataset
 Table1 <- rbind(Table_test, Table_train)
 
+## Creating Table2. Only selects the mean() and std()
 TblNames <- c("subject","labels", as.character(features[,2]))
-
 Table2 <- Table1[c(1,2,grep("mean\\(|std\\(",TblNames))]
 
-
+## Creating Table3. Names the labels with activity names.
 names(Table2) <- TblNames[c(1,2,grep("mean\\(|std\\(",TblNames))]
 Table3 <- Table2%>%
         merge(activities,by.x = "labels",by.y = "index", all = FALSE)%>%
         select(-labels)%>%
         arrange(subject, activities)
 
+## Creating Table 4. It is already created from last step 
 Table4 <- Table3
 
+## Creating Table 5.  
 Table5 <- Table4%>%
         gather(key = signal_parameter_coordinate, value, -subject, -activities)%>%
         separate(signal_parameter_coordinate, into = c("signal", "parameter", "coordinate"))%>%
         dcast(subject + activities + signal + coordinate ~ parameter, mean)%>%
         arrange(subject, activities)
 
-
+## Ordering signals in a organised way
 Table5$signal <- as.factor(Table5$signal)
 order_signal <- readLines("./UCI HAR Dataset/features_info.txt", skip = 23 )   
 order_signal <- data.frame(signal = order_signal[13:29])%>%
@@ -57,8 +54,8 @@ order_signal <- data.frame(signal = order_signal[13:29])%>%
         select(signal)
 levels(Table5$signal) <- order_signal$signal
 
-
-
+## Creating a text file
+write.table(Table5, "./UCI_tidy.txt", row.name = FALSE)
 
 
 
